58c7a0a2c454b15b083e617c630b0d833af42af6
miguel <python3developer@proton.me>
2025-06-17 12:44:40 +0800

    Added widget

---
+++ range_slider_widget/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Range Slider Widget",
+    "description": "Adds a slider widget for float fields",
+    "summary": 'Adds a slider widget for float fields',
+    "category": "web",
+    "version": "18.0.1.0.0",
+    "author": "Miguel Martinez Lopez",
+    "license": "AGPL-3",
+    "website": "https://github.com/holasoftware/odoo_range_slider_widget",
+    "depends": ["web"],
+    "license": "LGPL-3",
+    "assets": {
+        "web.assets_backend": [
+            'range_slider_widget/static/src/js/slider.js',
+            'range_slider_widget/static/src/xml/slider.xml',
+        ]
+    },
+    "auto_install": False,
+    "installable": True,
+}
