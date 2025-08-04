a52a4230ec65e28aa52a1ba3ced34df6377bf086
miguel <python3developer@proton.me>
2025-08-04 17:16:20 +0800

    Improved styling

--- range_slider_widget/__manifest__.py
+++ range_slider_widget/__manifest__.py
@@ -1,20 +1,19 @@
 {
     "name": "Range Slider Widget",
-    "description": "Adds a slider widget for float fields",
-    "summary": 'Adds a slider widget for float fields',
+    "summary": "Adds a slider widget for float fields",
     "category": "web",
     "version": "18.0.1.0.0",
-    "author": "Miguel Martinez Lopez",
+    "author": "Miguel Martinez Lopez, Odoo Community Association (OCA)",
     "license": "AGPL-3",
-    "website": "https://github.com/holasoftware/odoo_range_slider_widget",
+    "website": "https://github.com/OCA/web",
     "depends": ["web"],
-    "license": "LGPL-3",
     "assets": {
         "web.assets_backend": [
-            'range_slider_widget/static/src/js/slider.js',
-            'range_slider_widget/static/src/xml/slider.xml',
+            "range_slider_widget/static/src/js/slider.esm.js",
+            "range_slider_widget/static/src/xml/slider.xml",
         ]
     },
     "auto_install": False,
     "installable": True,
+    "maintainers": ["miguel-martinez-lopez"],
 }
